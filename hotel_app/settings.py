285ae117e1051a12c92c1e4ce4bae50b5079a216
Ex4i <wwudarczyk.exai@gmail.com>
2021-12-07 00:27:50 +0100

    STATIC_ROOT added to settings

--- hotel_app/settings.py
+++ hotel_app/settings.py
@@ -121,7 +121,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Django REST Framework settings
 
